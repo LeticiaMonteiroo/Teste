286aa038ad1d88fad2ad3cd48a3fc74954371b91
Letícia Monteiro <emaildeestudos90@gmail.com>
2024-11-06 17:43:40 -0300

    Continuação

--- orc-jogodavelha/src/Jogodavelha.js
+++ orc-jogodavelha/src/Jogodavelha.js
@@ -4,9 +4,41 @@ import './Jogodavelha.css';
 function Jogodavelha() {
   const emptyBoard = Array(9).fill("");  
   const [board, setBoard] = useState(emptyBoard);
+  const [currentPlayer, setCurrentPLayer] = useState("O");
+
+
   const handleCellClick = (index) => {
-    setBoard(board.map((item, itemIndex) => itemIndex == index ? "x" : item));
+
+    if(board[index] != "") return null;
+
+    setBoard(board.map((item, itemIndex) => itemIndex == index ? currentPlayer : item));
+
+
+    setCurrentPLayer(currentPlayer == "X" ? "O" : "X")
   }
+
+  const checkWinner{
+    
+    possibleWaystoWin[
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      [board[0], board[4], board[8]],
+      [board[6], board[4], board[6]],
+
+    ];
+
+    possibleWaystoWin.forEach
+  }
+
+
+
+
   return (
     <main>
       <h1 className="title">Jogo da Velha</h1>
